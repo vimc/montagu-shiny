d8573f7f32d5c2ae11a17fa20b0b45cecd6d2f3e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-01-03 16:26:59 +0000

    Use names that will work well in montagu

--- proxy/run.sh
+++ proxy/run.sh
@@ -3,8 +3,7 @@
 docker volume create montagu_shiny_volume
 docker network create testing
 docker run -d --rm \
-       --name montagu-shiny \
-       -p 3838:3838 \
+       --name shiny \
        -v montagu_shiny_volume:/srv/shiny-server/apps \
        --network=testing \
        docker.montagu.dide.ic.ac.uk:5000/montagu-shiny:master
