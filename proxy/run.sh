91cc01c98a0cc6a544bb726c0df09be8a3caba65
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-01-03 15:19:01 +0000

    JWT support

--- proxy/run.sh
+++ proxy/run.sh
@@ -9,8 +9,38 @@ docker run -d --rm \
        --network=testing \
        docker.montagu.dide.ic.ac.uk:5000/montagu-shiny:master
 
+# Get onto science, into the montagu_api_1 container and within
+# /etc/montagu/api/token_key/ run:
+openssl rsa -pubin -in public_key.der -inform DER -outform PEM
+
+# Save the contents of that as public_key.pem
+
+# Set the key this way, or set it with a path in the caddyfile, either work
+PUBLIC_KEY=$(cat public_key.pem)
 docker run --rm --name montagu-shiny-proxy \
        -p 1080:80 \
        -v ${PWD}/Caddyfile:/etc/Caddyfile \
+       -v ${PWD}/public_key.pem:/public_key.pem \
+       -e "JWT_PUBLIC_KEY=$PUBLIC_KEY" \
        --network=testing \
        montagu-shiny-proxy
+
+# Open https://support.montagu.dide.ic.ac.uk:11443/reports/ and login if needed
+#
+# Press F12 to open developer tools and F5 to refresh
+#
+# Select the reports/ item that corresponds to /reports/api/v1
+#
+# On Request headers select "view source"
+#
+# Copy the "Authorization" header
+#
+# Go to http://localahost:1080
+#
+# Press F12 for developer tools
+#
+# Add document.cookie="jwt_token=<token>" where "<token>" is the but
+# starting with eyJhb...
+#
+# Or curl with
+curl -v http://localhost:1080 -H "Authorization: Bearer eyJhb..."
